b5603a0ad3b562a7cf89b4b8a7a925bda3ba86fd
Logan Johnson <loganjson20@gmail.com>
2026-04-20 01:38:25 -0600

    Add new cmux command for long running functions

---
+++ dot_oh-my-zsh/custom/cmux.zsh
@@ -0,0 +1,10 @@
+notify-after() {
+  "$@"
+  local exit_code=$?
+  if [ $exit_code -eq 0 ]; then
+    cmux notify --title "✓ Command Complete" --body "$1"
+  else
+    cmux notify --title "✗ Command Failed" --body "$1 (exit $exit_code)"
+  fi
+  return $exit_code
+}
